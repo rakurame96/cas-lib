6bfe7744150c5e948ef1075acb1e8c18fd09eaaa
Mike Mulchrone <mtmulch0191@outlook.com>
2024-05-25 13:10:44 -0400

    ascon threadpool implementation

--- src/sponges/ascon_aead.rs
+++ src/sponges/ascon_aead.rs
@@ -1,4 +1,6 @@
 
+use std::sync::mpsc;
+
 use aes_gcm::AeadCore;
 use ascon_aead::{aead::{generic_array::GenericArray, Aead, KeyInit, OsRng}, Ascon128};
 
@@ -14,6 +16,16 @@ impl CASAsconAead for AsconAead {
         ciphertext
     }
 
+    fn encrypt_threadpool(key: Vec<u8>, nonce: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8> {
+        let (sender, receiver) = mpsc::channel();
+        rayon::spawn(move || {
+            let ciphertext = <AsconAead as CASAsconAead>::encrypt(key, nonce, plaintext);
+            sender.send(ciphertext);
+        });
+        let result = receiver.recv().unwrap();
+        result
+    }
+
     fn decrypt(key: Vec<u8>, nonce: Vec<u8>, ciphertext: Vec<u8>) -> Vec<u8> {
         let key_generic_array = GenericArray::from_slice(&key);
         let nonce_generic_array = GenericArray::from_slice(&nonce);
@@ -21,12 +33,42 @@ impl CASAsconAead for AsconAead {
         let plaintext = cipher.decrypt(&nonce_generic_array, ciphertext.as_ref()).unwrap();
         plaintext
     }
+
+    fn decrypt_threadpool(key: Vec<u8>, nonce: Vec<u8>, ciphertext: Vec<u8>) -> Vec<u8> {
+        let (sender, receiver) = mpsc::channel();
+        rayon::spawn(move || {
+            let plaintext = <AsconAead as CASAsconAead>::decrypt(key, nonce, ciphertext);
+            sender.send(plaintext);
+        });
+        let result = receiver.recv().unwrap();
+        result
+    }
     
     fn generate_key() -> Vec<u8> {
         return Ascon128::generate_key(&mut OsRng).to_vec();
     }
+
+    fn generate_key_threadpool() -> Vec<u8> {
+        let (sender, receiver) = mpsc::channel();
+        rayon::spawn(move || {
+            let key = <AsconAead as CASAsconAead>::generate_key();
+            sender.send(key);
+        });
+        let result = receiver.recv().unwrap();
+        result
+    }
     
     fn generate_nonce() -> Vec<u8> {
         return Ascon128::generate_nonce(&mut OsRng).to_vec();
+    }
+
+    fn generate_nonce_threadpool() -> Vec<u8> {
+        let (sender, receiver) = mpsc::channel();
+        rayon::spawn(move || {
+            let key = <AsconAead as CASAsconAead>::generate_nonce();
+            sender.send(key);
+        });
+        let result = receiver.recv().unwrap();
+        result
     }
 }
--- src/sponges/cas_ascon_aead.rs
+++ src/sponges/cas_ascon_aead.rs
@@ -1,6 +1,10 @@
 pub trait CASAsconAead {
     fn generate_key() -> Vec<u8>;
+    fn generate_key_threadpool() -> Vec<u8>;
     fn generate_nonce() -> Vec<u8>;
+    fn generate_nonce_threadpool() -> Vec<u8>;
     fn encrypt(key: Vec<u8>, nonce: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8>;
+    fn encrypt_threadpool(key: Vec<u8>, nonce: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8>;
     fn decrypt(key: Vec<u8>, nonce: Vec<u8>, ciphertext: Vec<u8>) -> Vec<u8>;
+    fn decrypt_threadpool(key: Vec<u8>, nonce: Vec<u8>, ciphertext: Vec<u8>) -> Vec<u8>;
 }
