c6b273b629f8843a5a759820f0f9413133a41903
Mike Mulchrone <mtmulch0191@outlook.com>
2024-07-21 10:21:19 -0400

    init for zstd

---
+++ src/compression/zstd.rs
@@ -0,0 +1,17 @@
+use std::io::{self, stdin, stdout, Cursor};
+
+
+
+fn compress(data_to_compress: Vec<u8>, level: i32) -> Vec<u8> {
+    let mut cursor = Cursor::new(data_to_compress);
+    let mut compressed_data = Vec::new();
+    zstd::stream::copy_encode(cursor, &mut compressed_data, level).unwrap();
+    compressed_data
+}
+
+fn decompress(data_to_decompress: Vec<u8>) -> Vec<u8> {
+    let mut cursor = Cursor::new(data_to_decompress);
+    let mut decompressed_data = Vec::new();
+    zstd::stream::copy_decode(&mut cursor, &mut decompressed_data).unwrap();
+    decompressed_data
+}
