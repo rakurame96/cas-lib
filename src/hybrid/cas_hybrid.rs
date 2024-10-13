705cab52714f21968ea50003efdef5be5e0a74e7
Mike Mulchrone <mikemulchrone987@gmail.com>
2024-10-13 12:22:44 -0700

    #3 encrypt hpke

--- src/hybrid/cas_hybrid.rs
+++ src/hybrid/cas_hybrid.rs
@@ -7,4 +7,5 @@ type Kdf = HkdfSha512;
 pub trait CASHybrid {
     fn generate_key_pair() -> (Vec<u8>, Vec<u8>, Vec<u8>);
     fn generate_info_str() -> Vec<u8>;
+    fn encrypt(plaintext: Vec<u8>, public_key: Vec<u8>, info_str: Vec<u8>) -> (Vec<u8>, Vec<u8>, Vec<u8>);
 }
--- src/hybrid/hpke.rs
+++ src/hybrid/hpke.rs
@@ -1,6 +1,10 @@
 use hpke::{
-    aead::AesGcm256, kdf::HkdfSha512, kem::X25519HkdfSha256, Kem as KemTrait, OpModeS, Serializable,
+    aead::{AeadTag, AesGcm256},
+    kdf::HkdfSha512,
+    kem::X25519HkdfSha256,
+    Deserializable, Kem as KemTrait, OpModeR, OpModeS, Serializable,
 };
+
 use rand::{rngs::StdRng, SeedableRng};
 use uuid::Uuid;
 
@@ -27,5 +31,20 @@ impl CASHybrid for CASHPKE {
         let uuid = Uuid::new_v4();
         let uuid_bytes: Vec<u8> = uuid.as_bytes().to_vec();
         uuid_bytes
+    }
+    
+    fn encrypt(plaintext: Vec<u8>, public_key: Vec<u8>, info_str: Vec<u8>) -> (Vec<u8>, Vec<u8>, Vec<u8>) {
+        let mut csprng = StdRng::from_entropy();
+        let public_key = <Kem as KemTrait>::PublicKey::from_bytes(public_key.as_slice())
+        .expect("could not deserialize server privkey!");
+        let (encapped_key, mut sender_ctx) =
+        hpke::setup_sender::<Aead, Kdf, Kem, _>(&OpModeS::Base, &public_key, &info_str.as_slice(), &mut csprng)
+            .expect("invalid server pubkey!");
+        let mut msg_copy = plaintext;
+        let tag = sender_ctx
+        .seal_in_place_detached(&mut msg_copy, b"")
+        .expect("encryption failed!");
+        let ciphertext = msg_copy;
+        (encapped_key.to_bytes().to_vec(), ciphertext, tag.to_bytes().to_vec())
     }
 }
